8876af33f4ffcbd2824f9760792cea7226df89bc
Alexander Schuch <alexander@schuch.me>
2015-01-31 01:13:31 +0100

    create Framework

---
+++ AwesomeCache/AwesomeCache.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeCache.h
+//  AwesomeCache
+//
+//  Created by Alexander Schuch on 31/01/15.
+//  Copyright (c) 2015 Alexander Schuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeCache.
+FOUNDATION_EXPORT double AwesomeCacheVersionNumber;
+
+//! Project version string for AwesomeCache.
+FOUNDATION_EXPORT const unsigned char AwesomeCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeCache/PublicHeader.h>
+
+
---
+++ AwesomeCacheTests/AwesomeCacheTests.swift
@@ -0,0 +1,136 @@
+//
+//  AwesomeCacheTests.swift
+//  AwesomeCacheTests
+//
+//  Created by Alexander Schuch on 31/01/15.
+//  Copyright (c) 2015 Alexander Schuch. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import AwesomeCache
+
+class AwesomeCacheTests: XCTestCase {
+    
+    var cache: AwesomeCache<NSString> = AwesomeCache<NSString>(name: "awesomeCache")
+    
+    override func setUp() {
+        cache = AwesomeCache<NSString>(name: "awesomeCache")
+        cache.removeAllObjects()
+        
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        cache.removeAllObjects()
+        
+        super.tearDown()
+    }
+    
+    func testGetterAndSetter() {
+        let nilObject = cache.objectForKey("unavailable")
+        XCTAssertNil(nilObject, "Get nil object")
+        
+        cache.setObject("AddedString", forKey: "add")
+        XCTAssertNotNil(cache.objectForKey("add"), "Get non-nil object")
+        XCTAssertEqual("AddedString", cache.objectForKey("add")!, "Get non-nil object")
+    }
+    
+    func testRemoveObject() {
+        cache.setObject("AddedString", forKey: "remove")
+        XCTAssertNotNil(cache.objectForKey("remove"), "Get non-nil object")
+        XCTAssertEqual("AddedString", cache.objectForKey("remove")!, "Get non-nil object")
+        
+        cache.removeObjectForKey("remove")
+        XCTAssertNil(cache.objectForKey("remove"), "Get deleted object")
+    }
+    
+    func testSubscripting() {
+        cache["addSubscript"] = "AddedString"
+        XCTAssertNotNil(cache["addSubscript"], "Get non-nil object via subscript")
+        XCTAssertEqual("AddedString", cache["addSubscript"]!, "Get non-nil object via subscript")
+        
+        cache["addSubscript"] = nil
+        XCTAssertNil(cache["addSubscript"], "Get deleted object via subscript")
+    }
+    
+    func testObjectExpiry() {
+        cache.setObject("NeverExpires", forKey: "never", expires: .Never)
+        cache.setObject("ExpiresIn2Seconds", forKey: "2Seconds", expires: .Seconds(2))
+        cache.setObject("ExpiresAtDate", forKey: "atDate", expires: .Date(NSDate().dateByAddingTimeInterval(4)))
+        
+        XCTAssertNotNil(cache.objectForKey("never"), "Never expires")
+        XCTAssertNotNil(cache.objectForKey("2Seconds"), "Expires in 2 seconds")
+        XCTAssertNotNil(cache.objectForKey("atDate"), "Expires in 4 seconds")
+        
+        sleep(2)
+        
+        XCTAssertNotNil(cache.objectForKey("never"), "Never expires")
+        XCTAssertNil(cache.objectForKey("2Seconds"), "Expires in 2 seconds")
+        XCTAssertNotNil(cache.objectForKey("atDate"), "Expires in 4 seconds")
+        
+        sleep(2)
+        
+        XCTAssertNotNil(cache.objectForKey("never"), "Never expires")
+        XCTAssertNil(cache.objectForKey("2Seconds"), "Expires in 2 seconds")
+        XCTAssertNil(cache.objectForKey("atDate"), "Expires in 3 seconds")
+    }
+    
+    func testCacheBlockExecuted() {
+        var executed = false
+        
+        cache.setObjectForKey("blockExecuted", cacheBlock: { successBlock, failureBlock in
+            executed = true
+            successBlock("AddedString", .Never)
+            }, completion: { object, isLoadedFromCache, error in
+                XCTAssertNotNil(object, "Cached object not nil")
+                XCTAssertEqual("AddedString", object!, "Get cached object")
+                
+                XCTAssertNotNil(self.cache.objectForKey("blockExecuted"), "Get cached object")
+                XCTAssertTrue(executed, "Block was executed")
+                XCTAssertFalse(isLoadedFromCache, "Object was not loaded cached")
+                XCTAssertNil(error, "Error is nil")
+        })
+    }
+    
+    func testCacheBlockNotExecuted() {
+        var executed = false
+        
+        cache.setObject("AddedString", forKey: "blockNotExecuted")
+        
+        cache.setObjectForKey("blockNotExecuted", cacheBlock: { successBlock, failureBlock in
+            executed = true
+            successBlock("SometingElse", .Never)
+            }, completion: { object, isLoadedFromCache, error in
+                XCTAssertNotNil(object, "Cached object not nil")
+                XCTAssertEqual("AddedString", object!, "Get cached object")
+                
+                XCTAssertNotNil(self.cache.objectForKey("blockNotExecuted"), "Get cached object")
+                XCTAssertEqual("AddedString", self.cache.objectForKey("blockNotExecuted")!, "Get cached object")
+                
+                XCTAssertFalse(executed, "Block was not executed")
+                XCTAssertTrue(isLoadedFromCache, "Object was loaded from cached")
+                XCTAssertNil(error, "Error is nil")
+        })
+    }
+    
+    
+    func testCacheBlockError() {
+        
+        cache.setObjectForKey("blockError", cacheBlock: { successBlock, failureBlock in
+            let error = NSError(domain: "AwesomeCacheErrorDomain", code: 42, userInfo: nil)
+            failureBlock(error)
+            }, completion: { object, isLoadedFromCache, error in
+                XCTAssertNil(object, "Cached object nil")
+                XCTAssertNil(self.cache.objectForKey("blockError"), "Get cached object")
+                
+                XCTAssertFalse(isLoadedFromCache, "Object was loaded from cached")
+                XCTAssertNotNil(error, "Error is nil")
+                XCTAssert(error!.domain == "AwesomeCacheErrorDomain", "Error domain")
+                XCTAssert(error!.code == 42, "Error code")
+        })
+        
+        
+    }
+
+}

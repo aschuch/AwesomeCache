793a2f85b543d22e844ec7b15b5f31929c6011c7
Denis Chaschin <denis@chaschin.pro>
2019-03-30 10:48:06 +1100

    `flatMap` updated to `compactMap`

--- AwesomeCache/Cache.swift
+++ AwesomeCache/Cache.swift
@@ -133,9 +133,9 @@ open class Cache<T: NSCoding> {
 
         queue.sync {
             let keys = self.allKeys()
-            let all = keys.map(self.read).flatMap { $0 }
+            let all = keys.map(self.read).compactMap { $0 }
             let filtered = includeExpired ? all : all.filter { !$0.isExpired() }
-            objects = filtered.map { $0.value as? T }.flatMap { $0 }
+            objects = filtered.map { $0.value as? T }.compactMap { $0 }
         }
 
         return objects
@@ -253,7 +253,7 @@ open class Cache<T: NSCoding> {
 
     fileprivate func allKeys() -> [String] {
         let urls = try? self.fileManager.contentsOfDirectory(at: self.cacheDirectory, includingPropertiesForKeys: nil, options: [])
-        return urls?.flatMap { $0.deletingPathExtension().lastPathComponent } ?? []
+        return urls?.compactMap { $0.deletingPathExtension().lastPathComponent } ?? []
     }
 
     fileprivate func urlForKey(_ key: String) -> URL {
